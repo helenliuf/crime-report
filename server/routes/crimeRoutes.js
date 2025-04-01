16ceb37480d353f3ceb341ac82a47439794ea29f
Helen Liu <helenfayliu@gmail.com>
2025-03-31 23:52:34 -0400

    Further authentication and more routes for crime controller funcs

--- server/routes/crimeRoutes.js
+++ server/routes/crimeRoutes.js
@@ -8,4 +8,9 @@ router.use(auth.authenticate); //Authentication
 router.get("/", CrimeController.getAllCrimes);
 router.get("/:id", CrimeController.getCrimeById);
 
+router.get("/", auth.authorize(["Police", "Admin"]), CrimeController.getAllCrimes);
+router.get("/:id", auth.authorize(["Citizen", "Police", "Admin"]), CrimeController.getCrimeById);
+router.get("/nearby", auth.authorize(["Police", "Admin"]), CrimeController.getNearbyCrimes);
+router.post("/", auth.authorize(["Citizen"]), CrimeController.addCrimeReport);
+
 module.exports = router;
