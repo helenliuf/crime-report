44153c9ffd53293ab569d175c0f38daa9cdf2432
Shaunak2003 <shaunak.karmarkar@gmail.com>
2025-05-08 14:08:52 -0400

    Updated controller and routes for rewards

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -65,10 +65,25 @@ exports.login = async (req, res) => {
 		  name: user.name,
 		  email: user.email,
 		  role: user.role,
+		  rewardPoints: user.rewardPoints // newly added
 		},
 	  });
 	} catch (error) {
 	  console.error("Login error:", error);
 	  res.status(500).json({ error: error.message });
+	}
+};
+
+// newly added
+exports.getRewardPoints = async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id);
+		if (!user) {
+			return res.status(404).json({ message: "User not found" });
+		}
+
+		res.status(200).json({ rewardPoints: user.rewardPoints });
+	} catch (error) {
+		res.status(500).json({ error: error.message });
 	}
 };
